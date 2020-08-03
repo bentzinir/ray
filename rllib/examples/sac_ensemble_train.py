e7294a88f5ec29efa4edb6515eb095434231af4d
bentzinir <bentzinir@gmail.com>
2020-08-03 11:19:38 +0300

    Merge branch 'master' of https://github.com/bentzinir/ray
    # Conflicts:
    #	rllib/examples/sac_ensemble_example.py
    
    1. Fixed active member bug - functionality change after pulling from master branch
    2. removed unnecessary manual callback
    
    3. Added new Maze environment
    4. Split main sac script to train and test

--- rllib/examples/sac_ensemble_train.py
+++ rllib/examples/sac_ensemble_train.py
@@ -4,38 +4,41 @@ from ray import tune
 from ray.rllib.agents.sac import SACEnsembleTrainer
 from ray.rllib.policy.sample_batch import SampleBatch
 import numpy as np
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--stop-iters", type=int, default=200)
-parser.add_argument("--num-cpus", type=int, default=0)
-parser.add_argument("--env", type=str, default="none")
-parser.add_argument("--debug", action="store_true")
-parser.add_argument("--tfe", action="store_true")
-parser.add_argument("--shared_actor", action="store_true")
-parser.add_argument("--ensemble_size", type=int, default=1)
-parser.add_argument("--timescale", type=int, default=10000)
-parser.add_argument("--timescale_grid_search", action="store_true")
-parser.add_argument("--timesteps", type=int, default=1000000)
-parser.add_argument("--verbose", type=int, default=1)
-parser.add_argument("--num_workers", type=int, default=1)
-parser.add_argument("--num_gpus", type=int, default=1)
-parser.add_argument("--gamma", type=float, default=0.99)
-parser.add_argument("--batch_size", type=int, default=256)
-parser.add_argument("--target_entropy", type=float, default=None)
-parser.add_argument("--ensemble_grid_search", action="store_true")
-parser.add_argument("--asymmetric", action="store_true")
-parser.add_argument("--local_mode", action="store_true")
-parser.add_argument("--experience_masking", action="store_true")
-parser.add_argument("--alpha", type=float, default=None)
-parser.add_argument("--alpha_grid_search", action="store_true")
-parser.add_argument("--local_dir", type=str, default="none")
-
 from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray.rllib.evaluation import MultiAgentEpisode, RolloutWorker
 from ray.rllib.env import BaseEnv
 from ray.rllib.policy import Policy
 from typing import Dict
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--stop-iters", type=int, default=200)
+    parser.add_argument("--num-cpus", type=int, default=0)
+    parser.add_argument("--env", type=str, default="none")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--tfe", action="store_true")
+    parser.add_argument("--shared_actor", action="store_true")
+    parser.add_argument("--ensemble_size", type=int, default=1)
+    parser.add_argument("--timescale", type=int, default=10000)
+    parser.add_argument("--timescale_grid_search", action="store_true")
+    parser.add_argument("--timesteps", type=int, default=1000000)
+    parser.add_argument("--verbose", type=int, default=1)
+    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--num_gpus", type=int, default=1)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--target_entropy", type=float, default=None)
+    parser.add_argument("--ensemble_grid_search", action="store_true")
+    parser.add_argument("--asymmetric", action="store_true")
+    parser.add_argument("--local_mode", action="store_true")
+    parser.add_argument("--experience_masking", action="store_true")
+    parser.add_argument("--alpha", type=float, default=None)
+    parser.add_argument("--alpha_grid_search", action="store_true")
+    parser.add_argument("--local_dir", type=str, default="none")
+    parser.add_argument("--checkpoint_dir", type=str, default=None)
+    parser.add_argument("--checkpoint_freq", type=int, default=0)
+    return parser
 
 
 def callback_builder():
@@ -76,28 +79,39 @@ def callback_builder():
     return MyCallbacks
 
 
-if __name__ == "__main__":
-    args, extra_args = parser.parse_known_args()
+def get_config(args):
     if args.experience_masking:
         batch_scale = args.ensemble_size
     else:
         batch_scale = 1
-    config = {
-            'env': args.env,
-            'num_workers': args.num_workers,
-            'num_gpus': args.num_gpus,
-            'partial_ensemble_size': tune.grid_search([5, 4, 3, 2, 1]) if args.ensemble_grid_search else args.ensemble_size,
-            'timescale': tune.grid_search([100, 1000, 10000, 50000]) if args.timescale_grid_search else args.timescale,
-            'shared_actor': args.shared_actor,
-            'framework': 'tfe' if args.tfe else 'tf',
-            'target_entropy': args.target_entropy,
-            'asymmetric': args.asymmetric,
-            "callbacks": callback_builder(),
-            'train_batch_size': batch_scale * args.batch_size,
-            'experience_masking': args.experience_masking,
-            'gamma': args.gamma,
-            'alpha': tune.grid_search([0.5, 0.4, 0.3, 0.2, 0.1]) if args.alpha_grid_search else args.alpha,
+    return {
+        'env': args.env,
+        'num_workers': args.num_workers,
+        'num_gpus': args.num_gpus,
+        'partial_ensemble_size': tune.grid_search([4, 3, 2, 1]) if args.ensemble_grid_search else args.ensemble_size,
+        'timescale': tune.grid_search([100, 1000, 10000, 50000]) if args.timescale_grid_search else args.timescale,
+        'shared_actor': args.shared_actor,
+        'framework': 'tfe' if args.tfe else 'tf',
+        'target_entropy': args.target_entropy,
+        'asymmetric': args.asymmetric,
+        "callbacks": callback_builder(),
+        'train_batch_size': batch_scale * args.batch_size,
+        'experience_masking': args.experience_masking,
+        'gamma': args.gamma,
+        'alpha': tune.grid_search([0.4, 0.3, 0.2, 0.1]) if args.alpha_grid_search else args.alpha,
     }
+
+
+def get_args():
+    args, extra_args = get_parser().parse_known_args()
+    if args.env == 'maze':
+        from ray.rllib.examples.env.maze_env import MazeEnv
+        args.env = MazeEnv
+    return args, extra_args
+
+
+if __name__ == "__main__":
+    args, extra_args = get_args()
 
     ray.init(num_cpus=args.num_cpus or None,
              local_mode=args.local_mode,
@@ -105,8 +119,9 @@ if __name__ == "__main__":
              # object_store_memory=int(10e9),
              # memory=int(16e9)
              )
+
     if args.debug:
-        trainer = SACEnsembleTrainer(config=config)
+        trainer = SACEnsembleTrainer(config=get_config(args))
         i = 0
         while True:
             results = trainer.train()  # distributed training step
@@ -114,7 +129,7 @@ if __name__ == "__main__":
     else:
         tune.run(SACEnsembleTrainer,
                  verbose=args.verbose,
-                 config=config,
+                 config=get_config(args),
                  stop={
                      "timesteps_total": args.timesteps,
                     },
@@ -123,4 +138,6 @@ if __name__ == "__main__":
                  #    },
                  reuse_actors=True,
                  local_dir=args.local_dir,
+                 checkpoint_freq=args.checkpoint_freq,
+                 checkpoint_at_end=True,
                  )
