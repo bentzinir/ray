1. sac_ma.py - changed default params to None's so as not to intervene with manual params 2. sac_ma_tf_policy.py - critical bug - normalizing observation for any integer type observation space 3. sac_ma_tf_policy.py - enable diversity penalty for next state sampled action 4. sac_ma_tf_policy.py - set default divergence_type to "none" to force the user to enter explicit value 5. sac_ma_tf_policy.py - entropy related measures are calculated based on "own experience" only and not based on other members 6. trainer.py - forcing action normalization based on the existence of "agents" attribute 7. maze_env.py - allowing a "spatial" mode that helps debug discrete Atari environments 8. multi_agent.py - manually implemented atari wrappers for non Atari "spatial" environments 9. sac_multiagent_test.py - added vis_sleep flag 10. sac_multiagent_train.py - loading default config from "tuned examples" params. 11. atari-sac.yaml - updated deprecated fields 12. maze-sac.yaml - added new configuration file 13. spatial-maze-sac.yaml - added new configuration file

rllib/agents/sac/sac_ma_tf_policy.py
@@ -111,10 +111,9 @@ def postprocess_trajectory(policy,
         agent_id = sample_batch["agent_id"][0]
         for i, member_id in enumerate(sample_batch['agent_id']):
             if member_id < agent_id:
-                # sample_batch["disc_valid"][i] = 1.
                 sample_batch["disc_label"][i] = OPPONENT_LABEL
+                sample_batch["own_experience"][i] = 0.
             elif member_id == agent_id:
-                # sample_batch["disc_valid"][i] = 1.
                 sample_batch["disc_label"][i] = AGENT_LABEL
                 sample_batch["own_experience"][i] = 1.
         if policy.config["shuffle_data"]:
@@ -155,18 +154,27 @@ def sac_actor_critic_loss(policy, model, _, train_batch):
     # Should be True only for debugging purposes (e.g. test cases)!
     deterministic = policy.config["_deterministic_loss"]
 
+    cur_obs = train_batch[SampleBatch.CUR_OBS]
+    next_obs = train_batch[SampleBatch.NEXT_OBS]
+
+    if policy.config["env_config"]["normalize_obs"]:
+        cur_obs = tf.cast(cur_obs, tf.float32) / 255.
+        next_obs = tf.cast(next_obs, tf.float32) / 255.
+    else:
+        print("====== No Obs Normalization ======")
+
     model_out_t, _ = model({
-        "obs": train_batch[SampleBatch.CUR_OBS],
+        "obs": cur_obs,
         "is_training": policy._get_is_training_placeholder(),
     }, [], None)
 
     model_out_tp1, _ = model({
-        "obs": train_batch[SampleBatch.NEXT_OBS],
+        "obs": next_obs,
         "is_training": policy._get_is_training_placeholder(),
     }, [], None)
 
     target_model_out_tp1, _ = policy.target_model({
-        "obs": train_batch[SampleBatch.NEXT_OBS],
+        "obs": next_obs,
         "is_training": policy._get_is_training_placeholder(),
     }, [], None)
 
@@ -200,14 +208,31 @@ def sac_actor_critic_loss(policy, model, _, train_batch):
         # Diversity reward
         if model.divergence_type == 'state_action':
             # calculated based on s_t, a_t. First extract discrimination for all actions in time t
-            discrimination = model.get_d_values(model_out_t, actions=None)
-            # Slice actually selected action a_t
-            selected_action_3d_mask = tf.tile(tf.expand_dims(one_hot, axis=1), multiples=[1, 2, 1])
-            discrimination = tf.reduce_sum(discrimination * selected_action_3d_mask, axis=2)
-        else:
+            at_time = 'tp1'
+            if at_time == 't':
+                discrimination = model.get_d_values(model_out_t, actions=None)
+                # Slice actually selected action a_t
+                one_hot_3d = tf.tile(tf.expand_dims(one_hot, axis=1), multiples=[1, 2, 1])
+                discrimination = tf.reduce_sum(discrimination * one_hot_3d, axis=2)
+            elif at_time == 'tp1':
+                discrimination = model.get_d_values(model_out_tp1, actions=None)
+                action_dist_class = get_dist_class(policy.config, policy.action_space)
+                action_dist_tp1 = action_dist_class(
+                    model.get_policy_output(model_out_tp1), policy.model)
+                sampled_a_tp1 = action_dist_tp1.sample()
+                # Slice sampled action a_tp1
+                one_hot_tp1 = tf.one_hot(sampled_a_tp1, depth=q_t.shape.as_list()[-1])
+                one_hot_3d_tp1 = tf.tile(tf.expand_dims(one_hot_tp1, axis=1), multiples=[1, 2, 1])
+                discrimination = tf.reduce_sum(discrimination * one_hot_3d_tp1, axis=2)
+            else:
+                raise ValueError
+        elif model.divergence_type == 'state':
             # calculated based on s_tp1
             discrimination = model.get_d_values(model_out_tp1, actions=None)
             discrimination = tf.squeeze(discrimination, axis=2)
+        else:
+            print(f"Unsupported divergence type: {model.divergence_type}")
+            raise ValueError
         log_discrimination = tf.nn.log_softmax(discrimination, axis=1)
         agent_discrimination_logit = tf.split(log_discrimination, 2, axis=1)[AGENT_LABEL]
         q_tp1_best += model.beta * tf.squeeze(agent_discrimination_logit, axis=1)
@@ -262,9 +287,12 @@ def sac_actor_critic_loss(policy, model, _, train_batch):
         if model.divergence_type == 'state_action':
             # calculate based on s_t, a_t
             discrimination = model.get_d_values(model_out_t, train_batch[SampleBatch.ACTIONS])
-        else:
+        elif model.divergence_type == 'state':
             # calculate based on s_tp1
             discrimination = model.get_d_values(model_out_tp1, actions=None)
+        else:
+            print(f"Unsupported divergence type: {model.divergence_type}")
+            raise ValueError
         discrimination = tf.squeeze(discrimination, axis=2)
         log_discrimination = tf.nn.log_softmax(discrimination, axis=1)
         agent_discrimination_logit = tf.split(log_discrimination, 2, axis=1)[AGENT_LABEL]
@@ -301,14 +329,19 @@ def sac_actor_critic_loss(policy, model, _, train_batch):
     # Note: In the papers, alpha is used directly, here we take the log.
     # Discrete case: Multiply the action probs as weights with the original
     # loss terms (no expectations needed).
+    own_exp_sum = 1e-4 + tf.reduce_sum(train_batch["own_experience"])
     if model.discrete:
-        alpha_loss = tf.reduce_mean(
+        alpha_loss = tf.reduce_sum(
             train_batch["own_experience"] * tf.reduce_sum(
                 tf.multiply(
                     tf.stop_gradient(policy_t), -model.log_alpha *
                     tf.stop_gradient(log_pis_t + model.target_entropy)),
-                axis=-1))
-        entropy = -tf.reduce_mean(tf.reduce_sum(tf.multiply(policy_t, log_pis_t), axis=-1))
+                axis=-1)) / own_exp_sum
+        entropy = -tf.reduce_sum(
+            train_batch["own_experience"] * tf.reduce_sum(tf.multiply(policy_t, log_pis_t), axis=-1)) / own_exp_sum
+        # action_dist_class = get_dist_class(policy.config, policy.action_space)
+        # action_dist_t = action_dist_class(model.get_policy_output(model_out_t), policy.model)
+        # entropy = action_dist_t.entropy()
         actor_loss = tf.reduce_mean(
             tf.reduce_sum(
                 tf.multiply(
@@ -318,17 +351,18 @@ def sac_actor_critic_loss(policy, model, _, train_batch):
                     model.alpha * log_pis_t - tf.stop_gradient(q_t)),
                 axis=-1))
     else:
-        alpha_loss = -tf.reduce_mean(
+        alpha_loss = -tf.reduce_sum(
             train_batch["own_experience"] * model.log_alpha *
-            tf.stop_gradient(log_pis_t + model.target_entropy))
-        entropy = -tf.reduce_mean(log_pis_t)
+            tf.stop_gradient(log_pis_t + model.target_entropy)) / own_exp_sum
+        entropy = -tf.reduce_sum(train_batch["own_experience"] * log_pis_t) / own_exp_sum
         actor_loss = tf.reduce_mean(model.alpha * log_pis_t - q_t_det_policy)
 
     # discrimination
     d_t = model.get_d_values(model_out_t, train_batch[SampleBatch.ACTIONS])
     if d_t.shape.as_list()[2] > 1:
         assert model.discrete
-        d_t = tf.reduce_sum(d_t * selected_action_3d_mask, axis=2)
+        one_hot_3d = tf.tile(tf.expand_dims(one_hot, axis=1), multiples=[1, 2, 1])
+        d_t = tf.reduce_sum(d_t * one_hot_3d, axis=2)
     else:
         d_t = tf.squeeze(d_t, axis=2)
     valid_count = 1e-4 + tf.reduce_sum(train_batch["disc_valid"])

rllib/examples/env/maze_env.py
@@ -45,8 +45,8 @@ class MazeEnv(gym.Env):
                                                      self.end_pos))
         self.h = len(self.map)
         self.w = len(self.map[0])
-        if "config" in env_config and env_config["spatial"]:
-            self.observation_space = Box(0, 255, shape=(42, 42, 3))
+        if "spatial" in env_config and env_config["spatial"]:
+            self.observation_space = Box(0, 255, shape=(42, 42, 3), dtype=np.uint8)
             self.spatial = True
         else:
             self.observation_space = Box(0, 100, shape=(2,))
@@ -69,14 +69,10 @@ class MazeEnv(gym.Env):
         self.pos = self.start_pos
         self.num_steps = 0
         if self.spatial:
-            return self._get_image()
+            return self._get_image(resize=True)
         return np.array(self.pos)
 
     def step(self, action, verbose=False):
-        # if action == 1:
-        #     self.pos = self._get_new_pos(self.pos, self.wind_direction)
-
-        # self.wind_direction = random.choice([0, 1, 2, 3])
         self.pos = self._get_new_pos(self.pos, action)
         self.num_steps += 1
         at_goal = self.pos == self.end_pos
@@ -86,7 +82,7 @@ class MazeEnv(gym.Env):
         # return (np.array(self.pos), float(10 * int(at_goal)), done, {})
         reward = 1. if at_goal else 0.
         if self.spatial:
-            obs = self._get_image()
+            obs = self._get_image(resize=True)
         else:
             obs = np.array(self.pos)
         return obs, reward, done, {}
@@ -124,7 +120,7 @@ class MazeEnv(gym.Env):
         else:
             raise ValueError
 
-    def _get_image(self, alpha=0.995):
+    def _get_image(self, resize=False, alpha=0.995):
         frame_t = self.bg.copy()
         frame_t[self.pos] =  [0, 255, 0] #self.member_color()
         # frame[self.end_pos] = [0, 0, 255]
@@ -135,7 +131,8 @@ class MazeEnv(gym.Env):
         # self.frame[self.pos] = self.member_color()
         frames = [frame_t] + [self.frames[i] for i in self.member_list]
         frames = np.concatenate(frames, axis=0)
-        frames = cv2.resize(frames, dsize=(42, 42), interpolation=cv2.INTER_NEAREST)
+        if resize:
+            frames = cv2.resize(frames, dsize=(42, 42), interpolation=cv2.INTER_NEAREST)
         return frames
 
     def render(self, mode='human'):

rllib/examples/sac_multiagent_train.py
@@ -10,6 +10,11 @@ from ray.rllib.env import BaseEnv
 from ray.rllib.policy import Policy
 from typing import Dict
 from ray.rllib.examples.env.multi_agent import make_multiagent
+import yaml
+import os
+from ray.rllib.env.atari_wrappers import is_atari
+import collections.abc
+import numpy as np
 
 
 def get_parser():
@@ -18,21 +23,19 @@ def get_parser():
     parser.add_argument("--num-cpus", type=int, default=0)
     parser.add_argument("--env", type=str, default="none")
     parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--tfe", action="store_true")
+    parser.add_argument("--framework", type=str, default="none")
     parser.add_argument("--shared_actor", action="store_true")
     parser.add_argument("--ensemble_size", type=int, default=1)
     parser.add_argument("--timescale", type=int, default=10000)
     parser.add_argument("--timescale_grid_search", action="store_true")
     parser.add_argument("--timesteps", type=int, default=1000000)
     parser.add_argument("--verbose", type=int, default=1)
-    parser.add_argument("--num_workers", type=int, default=1)
+    parser.add_argument("--num_workers", type=int, default=0)
     parser.add_argument("--num_gpus", type=int, default=1)
     parser.add_argument("--gamma", type=float, default=0.99)
     parser.add_argument("--batch_size", type=int, default=256)
-    parser.add_argument("--batch_scale", action="store_true")
     parser.add_argument("--target_entropy", type=float, default=None)
     parser.add_argument("--ensemble_grid_search", action="store_true")
-    parser.add_argument("--asymmetric", action="store_true")
     parser.add_argument("--local_mode", action="store_true")
     parser.add_argument("--alpha", type=float, default=None)
     parser.add_argument("--beta", type=float, default=None)
@@ -48,7 +51,17 @@ def get_parser():
     parser.add_argument("--shuffle_data", action="store_true")
     parser.add_argument("--divergence_type", type=str, default="none")
     parser.add_argument("--spatial", action="store_true")
+    parser.add_argument("--vis_sleep", type=float, default=0.0)
     return parser
+
+
+def update(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.abc.Mapping):
+            d[k] = update(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
 
 def callback_builder():
@@ -88,12 +101,6 @@ def central_critic_observer(agent_obs, **kw):
 
 
 def get_config(args):
-    if args.batch_scale:
-        batch_scale = args.ensemble_size
-        print(f"======Scaling Batch size: {args.ensemble_size}======")
-    else:
-        batch_scale = 1
-
     # Get obs- and action Spaces.
     if isinstance(args.env, str):
         single_env = gym.make(args.env)
@@ -102,35 +109,48 @@ def get_config(args):
     obs_space = single_env.observation_space
     act_space = single_env.action_space
 
+    if is_atari(single_env):
+        env_type = "atari"
+    elif args.env.__name__ == "MazeEnv":
+        if single_env.spatial:
+            env_type = "spatial-maze"
+        else:
+            env_type = "maze"
+    else:
+        env_type = "halfcheetah"
+    tuned_config_path = os.path.join(ray.__path__[0], "rllib/tuned_examples/sac", f"{env_type}-sac.yaml")
+    tuned_yaml = open(tuned_config_path)
+    tuned_config = yaml.load(tuned_yaml, Loader=yaml.FullLoader)
+
+    (k, tuned_config), = tuned_config.items()
+    config = tuned_config["config"]
+
     policies = {
         "policy_{}".format(i): (None, obs_space, act_space, {})
         for i in range(args.ensemble_size)
     }
 
-    return {
+    override_config = {
         'env': make_multiagent(args.env),
         "env_config": {
             "num_agents": args.ensemble_size,
-            "normalize_actions": True,
-            "asymmetric": args.asymmetric,
             "spatial": args.spatial,
+            # We normalize by 255 observations of any integer type
+            "normalize_obs": np.issubdtype(single_env.observation_space.dtype, np.integer),
             },
         'num_workers': args.num_workers,
         'num_gpus': args.num_gpus,
-        'framework': 'tfe' if args.tfe else 'tf',
+        'framework': args.framework,
         'target_entropy': args.target_entropy,
         "callbacks": callback_builder(),
-        'train_batch_size': batch_scale * args.batch_size,
         "multiagent": {
             "policies": policies,
             "policy_mapping_fn": (lambda x: f"policy_{x}"),
             "observation_fn": central_critic_observer,
         },
-        "normalize_actions": False,
         "alpha": args.alpha,
         "beta": args.beta,
         'gamma': args.gamma,
-        "asymmetric": args.asymmetric,
         "buffer_size": args.buffer_size,
         "entropy_scale": args.entropy_scale,
         "target_acc": args.target_acc,
@@ -140,7 +160,10 @@ def get_config(args):
             "beta_learning_rate": args.beta_learning_rate,
         },
         "divergence_type": args.divergence_type,
+        "prioritized_replay": False,  # TODO: consider setting back to True
     }
+    config = update(config, override_config)
+    return config
 
 
 def get_args():