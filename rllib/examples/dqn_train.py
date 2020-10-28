552a70db131b744be628eb06e3f6f06f75d9f9d0
bentzinir <bentzinir@gmail.com>
2020-10-28 13:25:28 +0200

    -----DIRTY COMMIT-----

--- rllib/examples/dqn_train.py
+++ rllib/examples/dqn_train.py
@@ -2,7 +2,7 @@ import argparse
 import gym
 import ray
 from ray import tune
-from ray.rllib.agents.dqn import DQNMATrainer
+from ray.rllib.agents.dqn import DQNTrainer
 from ray.rllib.policy.sample_batch import SampleBatch
 from ray.rllib.agents.callbacks import DefaultCallbacks
 from ray.rllib.evaluation import MultiAgentEpisode, RolloutWorker
@@ -17,44 +17,40 @@ import numpy as np
 from ray.rllib.models.tf.visionnet import VisionNetwork
 
 
-def base_model_init():
-    global BASE_MODEL
-    BASE_MODEL = {"main": None, "target": None}
-
-
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--f", type=str, default="none")
-    # parser.add_argument("--stop-iters", type=int, default=200)
-    # parser.add_argument("--num_workers", type=int, default=0)
-    # parser.add_argument("--num_gpus", type=int, default=1)
-    # parser.add_argument("--env", type=str, default="none")
-    # parser.add_argument("--debug", action="store_true")
-    # parser.add_argument("--framework", type=str, default="tf")
-    # parser.add_argument("--shared_base_model", action="store_true")
-    # parser.add_argument("--ensemble_size", type=int, default=1)
-    # parser.add_argument("--timescale", type=int, default=10000)
-    # parser.add_argument("--timesteps", type=int, default=1000000)
-    # parser.add_argument("--verbose", type=int, default=1)
-    # parser.add_argument("--gamma", type=float, default=0.99)
-    # parser.add_argument("--batch_size", type=int, default=256)
-    # parser.add_argument("--target_entropy", type=float, default=None)
-    # parser.add_argument("--grid_search", action="store_true")
-    # parser.add_argument("--local_mode", action="store_true")
-    # parser.add_argument("--beta", type=float, default=None)
-    # parser.add_argument("--local_dir", type=str, default="none")
-    # parser.add_argument("--checkpoint_dir", type=str, default=None)
-    # parser.add_argument("--checkpoint_freq", type=int, default=0)
-    # parser.add_argument("--buffer_size", type=int, default=50000)
-    # parser.add_argument("--entropy_scale", type=float, default=1.)
-    # parser.add_argument("--target_div", type=float, default=None)
-    # parser.add_argument("--entropy_lr", type=float, default=5e-4)
-    # parser.add_argument("--beta_learning_rate", type=float, default=3e-4)
-    # parser.add_argument("--shuffle_data", action="store_true")
-    # parser.add_argument("--divergence_type", type=str, default="none")
-    # parser.add_argument("--vis_sleep", type=float, default=0.0)
-    # parser.add_argument("--initial_beta", type=float, default=0.0001)
-    # parser.add_argument("--object_store_memory", type=int, default=1000000000)
+    parser.add_argument("--stop-iters", type=int, default=200)
+    parser.add_argument("--num-cpus", type=int, default=0)
+    parser.add_argument("--num_workers", type=int, default=0)
+    parser.add_argument("--num_gpus", type=int, default=1)
+    parser.add_argument("--env", type=str, default="none")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--framework", type=str, default="tf")
+    parser.add_argument("--shared_base_model", action="store_true")
+    parser.add_argument("--ensemble_size", type=int, default=1)
+    parser.add_argument("--timescale", type=int, default=10000)
+    parser.add_argument("--timesteps", type=int, default=1000000)
+    parser.add_argument("--verbose", type=int, default=1)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--target_entropy", type=float, default=None)
+    parser.add_argument("--ensemble_grid_search", action="store_true")
+    parser.add_argument("--local_mode", action="store_true")
+    parser.add_argument("--beta", type=float, default=None)
+    parser.add_argument("--local_dir", type=str, default="none")
+    parser.add_argument("--checkpoint_dir", type=str, default=None)
+    parser.add_argument("--checkpoint_freq", type=int, default=0)
+    parser.add_argument("--buffer_size", type=int, default=50000)
+    parser.add_argument("--entropy_scale", type=float, default=1.)
+    parser.add_argument("--target_div", type=float, default=None)
+    parser.add_argument("--entropy_lr", type=float, default=5e-4)
+    parser.add_argument("--beta_learning_rate", type=float, default=3e-4)
+    parser.add_argument("--shuffle_data", action="store_true")
+    parser.add_argument("--divergence_type", type=str, default="none")
+    parser.add_argument("--vis_sleep", type=float, default=0.0)
+    parser.add_argument("--yaml_config", type=str, default="none")
+    parser.add_argument("--initial_beta", type=float, default=0.0001)
+    parser.add_argument("--object_store_memory", type=int, default=1000000000)
     return parser
 
 
@@ -101,30 +97,22 @@ def callback_builder():
 
 
 def get_config(args):
-    # Get obs and action Spaces.
+    # Get obs- and action Spaces.
     if isinstance(args.env, str):
         single_env = gym.make(args.env)
-    elif isinstance(args.env, dict) and args.env.get('grid_search', False):  # grid search mode
-        single_env = gym.make(args.env['grid_search'][0])
-    elif callable(args.env):
-        single_env = args.env()
-    obs_space = single_env.observation_space
-    act_space = single_env.action_space
-
-    if isinstance(args.ensemble_size, int):
-        num_policies = args.ensemble_size
     else:
-        num_policies = max(args.ensemble_size['grid_search'])
+        single_env = args.env({"spatial": args.spatial})
+    obs_space = single_env.observation_space
+    # assert args.yaml_config != "none"
+    # tuned_yaml = open(args.yaml_config)
+    # tuned_config = yaml.load(tuned_yaml, Loader=yaml.FullLoader)
 
-    policies = {
-        "policy_{}".format(i): (None, obs_space, act_space, {})
-        for i in range(num_policies)
-    }
+    # (k, tuned_config), = tuned_config.items()
+    # config = tuned_config["config"]
 
     config = {
-        'env': make_multiagent(args.env),
+        'env': args.env,
         "env_config": {
-            "num_agents": args.ensemble_size,
             # We normalize observations of any integer type by 255
             "normalize_obs": np.issubdtype(single_env.observation_space.dtype, np.integer),
             },
@@ -132,19 +120,10 @@ def get_config(args):
         'num_gpus': args.num_gpus,
         'framework': args.framework,
         "callbacks": callback_builder(),
-        "multiagent": {
-            "policies": policies,
-            "policy_mapping_fn": (lambda x: f"policy_{x[0]}"),
-        },
-        "beta": args.beta,
-        "initial_beta": args.initial_beta,
         'gamma': args.gamma,
         "buffer_size": args.buffer_size,
-        "entropy_lr": args.entropy_lr,
-        "target_div": args.target_div,
-        "divergence_type": args.divergence_type,
         "compress_observations": np.issubdtype(single_env.observation_space.dtype, np.integer),
-        "learning_starts": 0,  # args.learning_starts,
+        "learning_starts": 0,
         "model": {
             "custom_model": None if isinstance(obs_space, gym.spaces.Discrete) or len(
                 obs_space.shape) <= 2 else VisionNetwork,
@@ -153,32 +132,38 @@ def get_config(args):
             }
         }
     }
+
     return config
 
 
 def get_args():
     args, extra_args = get_parser().parse_known_args()
-    if args.f is not "none":
-        config_file = open(args.f)
-        params = yaml.load(config_file, Loader=yaml.FullLoader)
-    return argparse.Namespace(**params)
+    if args.env == 'maze':
+        args.env = MazeEnv
+        args.spatial = False
+    elif args.env == 'maze-spatial':
+        args.env = MazeEnv
+        args.spatial = True
+    else:
+        args.spatial = False
+    return args, extra_args
 
 
 if __name__ == "__main__":
-    args = get_args()
+    args, extra_args = get_args()
 
     ray.init(num_cpus=args.num_cpus or None,
              local_mode=args.local_mode,
              object_store_memory=args.object_store_memory)
 
     if args.debug:
-        trainer = DQNMATrainer(config=get_config(args))
+        trainer = DQNTrainer(config=get_config(args))
         i = 0
         while True:
             results = trainer.train()
             print(f"Iter: {results['training_iteration']}, R: {results['episode_reward_mean']}")
     else:
-        tune.run(DQNMATrainer,
+        tune.run(DQNTrainer,
                  verbose=args.verbose,
                  config=get_config(args),
                  stop={"timesteps_total": args.timesteps},
